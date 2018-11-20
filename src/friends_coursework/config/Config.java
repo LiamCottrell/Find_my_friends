e524a8d63353c6dc232beb362aef64f9d5a3ab9d
Liam Cottrell <liamcottrell@hotmail.co.uk>
2018-11-20 11:37:53 +0000

    Ammended config for remote AWS

--- src/friends_coursework/config/Config.java
+++ src/friends_coursework/config/Config.java
@@ -10,6 +10,6 @@ public static final String DYNAMODB_TABLE_NAME="cm4108-lab06-city";
 // AWS Region. Refer to API to see what regions are available.
 // *** To use a local server, set this to "local". ***
 //
-public static final String REGION="local";
-public static final String LOCAL_ENDPOINT="http://localhost:8000";
+public static final String REGION="eu-west-2";
+public static final String LOCAL_ENDPOINT="apigateway.eu-west-2.amazonaws.com";
 } //end class
