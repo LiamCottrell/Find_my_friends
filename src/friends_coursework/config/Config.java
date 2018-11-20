60fb31065a330c4d5288e3255c90bf7c720e6752
Liam Cottrell <liamcottrell@hotmail.co.uk>
2018-11-20 11:51:19 +0000

    Table names implemented for new schema
    Table names added to config, ready to be incorporated into planned implementation

--- src/friends_coursework/config/Config.java
+++ src/friends_coursework/config/Config.java
@@ -3,9 +3,12 @@ package friends_coursework.config;
 public class Config
 {
 //
-// table name in DynamoDB
+// table names in DynamoDB
 //
-public static final String DYNAMODB_TABLE_NAME="cm4108-lab06-city";
+public static final String DYNAMODB_TABLE_NAME="cm4108-lab06-city"; //remove later
+public static final String DYNAMODB_USERS_TABLE_NAME="dev-cm4108-friends-users";
+public static final String DYNAMODB_LOCATIONS_TABLE_NAME="dev-cm4108-friends-locations";
+public static final String DYNAMODB_SUBSCRIPIONS_TABLE_NAME="dev-cm4108-friends-subscriptions";
 //
 // AWS Region. Refer to API to see what regions are available.
 // *** To use a local server, set this to "local". ***
